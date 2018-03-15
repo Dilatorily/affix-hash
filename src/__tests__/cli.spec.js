45b6382575f71be900463148adcdb380b0f4bc8d
Huy Dang Lê-Ngô <ngohu@amazon.com>
2018-03-15 13:35:49 -0700

    Trying to remove the temp folder

--- src/__tests__/cli.spec.js
+++ src/__tests__/cli.spec.js
@@ -4,14 +4,11 @@ const rimraf = require('rimraf');
 
 describe('cli', () => {
   beforeEach(() => {
-    fs.mkdirSync('tests');
-    fs.writeFileSync('tests/example-file.txt', 'Hello World!');
-    fs.writeFileSync('tests/example-file.md', '# Some Markdown');
-    fs.writeFileSync('tests/.ignored', 'Look, no extension!');
-  });
-
-  afterEach(() => {
-    rimraf.sync('tests');
+    rimraf.sync('temp');
+    fs.mkdirSync('temp');
+    fs.writeFileSync('temp/example-file.txt', 'Hello World!');
+    fs.writeFileSync('temp/example-file.md', '# Some Markdown');
+    fs.writeFileSync('temp/.ignored', 'Look, no extension!');
   });
 
   it('shows the help menu if we don\'t pass any parameters', async () => {
@@ -20,35 +17,35 @@ describe('cli', () => {
     } catch (error) {
       expect(error.code).toBe(2);
       expect(error.stdout).toBe('\n  Small CLI tool to affix a hash to filenames\n\n  Usage:\n    $ affix-hash <path|glob> [options]\n\n  Options:\n    -a, --hash-algorithm  Hash algorithm used. Uses the same list as crypto.createHash. (Default: md5)\n                          https://nodejs.org/api/crypto.html#crypto_crypto_createhash_algorithm_options\n    -b, --buffer-length   Length in bytes of the buffer used to read the files. (Default: 8192)\n    -d, --dry-run         List what would be renamed instead of renaming.\n    -l, --hash-length     Length of the appended MD5 hash. (Default: 20)\n    -m, --manifest        Creates a manifest of the renamed files. (Default: false)\n    -n, --manifest-name   Name of the manifest file. (Default: manifest.json)\n\n  Examples:\n    $ affix-hash main.css -a sha512 -l 25\n    $ affix-hash \'**.md\' \'!node_modules\' --dry-run\n\n');
-      expect(fs.existsSync('tests/example-file.txt')).toBe(true);
-      expect(fs.existsSync('tests/example-file.md')).toBe(true);
-      expect(fs.existsSync('tests/.ignored')).toBe(true);
+      expect(fs.existsSync('temp/example-file.txt')).toBe(true);
+      expect(fs.existsSync('temp/example-file.md')).toBe(true);
+      expect(fs.existsSync('temp/.ignored')).toBe(true);
     }
   });
 
   it('affixes a hash to filenames', async () => {
-    await execa('./src/cli.js', ['tests/**/*']);
-    expect(fs.existsSync('tests/example-file.txt')).toBe(false);
-    expect(fs.existsSync('tests/example-file.md')).toBe(false);
-    expect(fs.existsSync('tests/.ignored')).toBe(true);
+    await execa('./src/cli.js', ['temp/**/*']);
+    expect(fs.existsSync('temp/example-file.txt')).toBe(false);
+    expect(fs.existsSync('temp/example-file.md')).toBe(false);
+    expect(fs.existsSync('temp/.ignored')).toBe(true);
   });
 
   it('creates a manifest file of the renamed files', async () => {
-    await execa('./src/cli.js', ['tests/**/*', '--manifest']);
+    await execa('./src/cli.js', ['temp/**/*', '--manifest']);
     expect(fs.existsSync('manifest.json')).toBe(true);
     rimraf.sync('manifest.json');
   });
 
   it('uses the configured manifest filename', async () => {
-    await execa('./src/cli.js', ['tests/**/*', '--manifest', '--manifest-name', 'test.json']);
+    await execa('./src/cli.js', ['temp/**/*', '--manifest', '--manifest-name', 'test.json']);
     expect(fs.existsSync('test.json')).toBe(true);
     rimraf.sync('test.json');
   });
 
   it('does not modify the files with the dry run parameter', async () => {
-    await execa('./src/cli.js', ['\'tests/**/*\'', '--dry-run']);
-    expect(fs.existsSync('tests/example-file.txt')).toBe(true);
-    expect(fs.existsSync('tests/example-file.md')).toBe(true);
-    expect(fs.existsSync('tests/.ignored')).toBe(true);
+    await execa('./src/cli.js', ['\'temp/**/*\'', '--dry-run']);
+    expect(fs.existsSync('temp/example-file.txt')).toBe(true);
+    expect(fs.existsSync('temp/example-file.md')).toBe(true);
+    expect(fs.existsSync('temp/.ignored')).toBe(true);
   });
 });
--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -4,59 +4,56 @@ const rimraf = require('rimraf');
 
 describe('affix-hash', () => {
   beforeEach(() => {
-    fs.mkdirSync('tests');
-    fs.writeFileSync('tests/example-file.txt', 'Hello World!');
-    fs.writeFileSync('tests/example-file.md', '# Some Markdown');
-    fs.writeFileSync('tests/.ignored', 'Look, no extension!');
-  });
-
-  afterEach(() => {
-    rimraf.sync('tests');
+    rimraf.sync('temp');
+    fs.mkdirSync('temp');
+    fs.writeFileSync('temp/example-file.txt', 'Hello World!');
+    fs.writeFileSync('temp/example-file.md', '# Some Markdown');
+    fs.writeFileSync('temp/.ignored', 'Look, no extension!');
   });
 
   it('affixes a hash to filenames', async () => {
-    const filenames = await affixHash('tests/**/*.txt');
-    expect(filenames).toEqual({ 'tests/example-file.txt': 'tests/example-file.ed076287532e86365e84.txt' });
+    const filenames = await affixHash('temp/**/*.txt');
+    expect(filenames).toEqual({ 'temp/example-file.txt': 'temp/example-file.ed076287532e86365e84.txt' });
   });
 
   it('has default values for its options parameter', async () => {
-    const filenames = await affixHash('tests/**/*.txt', {});
-    expect(filenames).toEqual({ 'tests/example-file.txt': 'tests/example-file.ed076287532e86365e84.txt' });
+    const filenames = await affixHash('temp/**/*.txt', {});
+    expect(filenames).toEqual({ 'temp/example-file.txt': 'temp/example-file.ed076287532e86365e84.txt' });
   });
 
   it('works with an array of patterns', async () => {
-    const filenames = await affixHash(['tests/**/*', '!tests/**/*.md']);
-    expect(filenames).toEqual({ 'tests/example-file.txt': 'tests/example-file.ed076287532e86365e84.txt' });
+    const filenames = await affixHash(['temp/**/*', '!temp/**/*.md']);
+    expect(filenames).toEqual({ 'temp/example-file.txt': 'temp/example-file.ed076287532e86365e84.txt' });
   });
 
   it('works with a tiny buffer length', async () => {
-    const filenames = await affixHash('tests/**/*.txt', { bufferLength: 1 });
-    expect(filenames).toEqual({ 'tests/example-file.txt': 'tests/example-file.ed076287532e86365e84.txt' });
+    const filenames = await affixHash('temp/**/*.txt', { bufferLength: 1 });
+    expect(filenames).toEqual({ 'temp/example-file.txt': 'temp/example-file.ed076287532e86365e84.txt' });
   });
 
   it('works with a large buffer length', async () => {
-    const filenames = await affixHash('tests/**/*.txt', { bufferLength: 32768 });
-    expect(filenames).toEqual({ 'tests/example-file.txt': 'tests/example-file.ed076287532e86365e84.txt' });
+    const filenames = await affixHash('temp/**/*.txt', { bufferLength: 32768 });
+    expect(filenames).toEqual({ 'temp/example-file.txt': 'temp/example-file.ed076287532e86365e84.txt' });
   });
 
   // TODO: Find a good way to test hashing a large file
   xit('works with a large file', async () => {
-    const filenames = await affixHash('tests/**/*.bin');
-    expect(filenames).toEqual({ 'tests/example-file.bin': 'tests/example-file.TODO.bin' });
+    const filenames = await affixHash('temp/**/*.bin');
+    expect(filenames).toEqual({ 'temp/example-file.bin': 'temp/example-file.TODO.bin' });
   });
 
   it('hashes using a different hash algorithm', async () => {
-    const filenames = await affixHash('tests/**/*.txt', { hashAlgorithm: 'sha512' });
-    expect(filenames).toEqual({ 'tests/example-file.txt': 'tests/example-file.861844d6704e8573fec3.txt' });
+    const filenames = await affixHash('temp/**/*.txt', { hashAlgorithm: 'sha512' });
+    expect(filenames).toEqual({ 'temp/example-file.txt': 'temp/example-file.861844d6704e8573fec3.txt' });
   });
 
   it('concatenates the hash length to the desired length', async () => {
-    const filenames = await affixHash('tests/**/*.txt', { hashLength: 10 });
-    expect(filenames).toEqual({ 'tests/example-file.txt': 'tests/example-file.ed07628753.txt' });
+    const filenames = await affixHash('temp/**/*.txt', { hashLength: 10 });
+    expect(filenames).toEqual({ 'temp/example-file.txt': 'temp/example-file.ed07628753.txt' });
   });
 
   it('outputs the maximum hash length if the desired length is greater than it', async () => {
-    const filenames = await affixHash('tests/**/*.txt', { hashLength: 1000 });
-    expect(filenames).toEqual({ 'tests/example-file.txt': 'tests/example-file.ed076287532e86365e841e92bfc50d8c.txt' });
+    const filenames = await affixHash('temp/**/*.txt', { hashLength: 1000 });
+    expect(filenames).toEqual({ 'temp/example-file.txt': 'temp/example-file.ed076287532e86365e841e92bfc50d8c.txt' });
   });
 });
